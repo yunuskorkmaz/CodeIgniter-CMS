b53c4e89684b520da58111ae46228fc3071a493f
Yunus Korkmaz <Yunus Korkmaz>
2016-11-14 12:45:17 +0300

    First Upload

---
+++ application/views/theme/footer.php
@@ -0,0 +1,28 @@
+<!-- #END# With Material Design Colors -->
+</div>
+</section>
+
+
+<!-- Jquery Core Js -->
+<script src="<?php echo base_url(); ?>assets/plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.js"></script>
+
+<!-- Select Plugin Js -->
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+<!-- Slimscroll Plugin Js -->
+<script src="<?php echo base_url(); ?>assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+<!-- Waves Effect Plugin Js -->
+<script src="<?php echo base_url(); ?>assets/plugins/node-waves/waves.js"></script>
+
+<!-- Custom Js -->
+<script src="<?php echo base_url(); ?>assets/js/admin.js"></script>
+
+<!-- Demo Js -->
+<script src="<?php echo base_url(); ?>assets/js/demo.js"></script>
+</body>
+
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,12 @@
+<?php
+
+
+include "theme/head.php";
+include "theme/navbar.php";
+include "theme/sidebar.php";
+include "theme/content.php";
+include "theme/footer.php";
+
+
+
+?>
